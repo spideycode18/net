faf9efb28dc29924daac71ecd9db6bb405ae3978
spideycode <spideycode18@gmail.com>
2024-01-30 12:04:14 +0700

    update gitignore

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e14776438250373d085fbe0170fa6bd60b9e47")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

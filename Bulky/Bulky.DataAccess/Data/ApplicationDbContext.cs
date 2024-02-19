b63241742671970d1eff3114e190ca7e50fc9913
spideycode <spideycode18@gmail.com>
2024-02-20 01:08:40 +0700

    update N-tier

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
 ﻿using BulkyWeb.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyWeb.Data
+namespace Bulky.DataAccess.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: DbContext    
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
         }
